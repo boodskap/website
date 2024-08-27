d81cd79ad84d93c697533d8b87d165f9f3ccbf41
jeganvicent <jvincent@boodskap.io>
2024-08-27 12:42:23 +0530

    latest tag added

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 VERSION=1.2.4
-docker build -t boodskapiot/website:$VERSION .
+docker build -t boodskapiot/website:$VERSION -t boodskapiot/website:latest .
 docker push boodskapiot/website:$VERSION
+docker push boodskapiot/website:latest
