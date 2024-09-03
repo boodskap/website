cad15ea7e13f73d91580accee35a71e619f1f139
jeganvicent <jvincent@boodskap.io>
2024-09-03 09:38:20 +0530

    upgraded version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.2.4
+VERSION=1.2.5
 docker build -t boodskapiot/website:$VERSION -t boodskapiot/website:latest .
 docker push boodskapiot/website:$VERSION
 docker push boodskapiot/website:latest
